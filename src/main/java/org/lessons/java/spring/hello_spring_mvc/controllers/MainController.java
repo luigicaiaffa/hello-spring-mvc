2982fac61f7f82bf5b770227f8a583e8cfa4e20e
luigicaiaffa <luigicaiaffa8@gmail.com>
2025-03-18 15:10:19 +0100

    Add GreetingPage

--- src/main/java/org/lessons/java/spring/hello_spring_mvc/controllers/MainController.java
+++ src/main/java/org/lessons/java/spring/hello_spring_mvc/controllers/MainController.java
@@ -1,8 +1,10 @@
 package org.lessons.java.spring.hello_spring_mvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -12,7 +14,10 @@ public class MainController {
     public String getHomePage() {
         return "index";
     }
-    
 
-
+    @GetMapping("/greeting")
+    public String getGreetingPage(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
 }
